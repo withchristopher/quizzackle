214d44ee3956e8db8e0e76416aa796459874bbe5
Chris Maxwell <chmaxwel@icloud.com>
2020-06-13 17:00:54 +0200

    folder change

---
+++ assets/quiz.js
@@ -0,0 +1,19 @@
+// Quiz Game for testing Javascript students
+
+// Parameters for name, time, 
+var quizHScore = document.getElementById("quizHScore")
+var timeStart = 100;
+var studentName = "Tony";
+
+//Quizz Function to begin
+document.getElementById("quizStart").addEventListener("click", function startQuiz() {
+
+        }
+        //function for timer
+        var timer = function() {};
+
+        // function for Javascript questions
+        var questions = function() {};
+
+        //function for results
+        var resut = function() {};
