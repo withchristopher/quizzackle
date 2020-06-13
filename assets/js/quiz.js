7510de52a987b6b7d73cfabf54eab56e03eb6368
Chris Maxwell <chmaxwel@icloud.com>
2020-06-13 13:12:33 +0200

    html and js folder

---
+++ assets/js/quiz.js
@@ -0,0 +1,12 @@
+// Quiz Game for testing Javascript students
+
+// Parameters for name, time, 
+
+var timeStart = 60;
+var studentName = window.prompt("What is your name");
+
+//function for timer
+
+// function for Javascript questions
+
+//function for results
