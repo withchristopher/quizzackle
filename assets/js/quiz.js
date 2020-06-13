690d875699d0d4d423901efbf9fffefb92f831e9
Chris Maxwell <chmaxwel@icloud.com>
2020-06-13 13:15:40 +0200

    psuedo write up

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -6,7 +6,10 @@ var timeStart = 60;
 var studentName = window.prompt("What is your name");
 
 //function for timer
+var timer = function() {};
 
 // function for Javascript questions
+var questions = function() {};
 
 //function for results
+var resut = function() {};
